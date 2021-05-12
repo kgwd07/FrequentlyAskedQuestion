a762dff45518d146cc22cce38e5afd6fe05276f0
kiran <kgwd07@gmail.com>
2021-05-12 19:55:53 +0530

    Updates

---
+++ precache-manifest.baeba32fae192c77feaf7dc3ab3f594e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51226577f4a9a2fdb07555ae1e8acd17",
+    "url": "/FrequentlyAskedQuestion/index.html"
+  },
+  {
+    "revision": "6f9ec453e6c593c3c7d4",
+    "url": "/FrequentlyAskedQuestion/static/css/main.19025435.chunk.css"
+  },
+  {
+    "revision": "ab85a117f39d501e282e",
+    "url": "/FrequentlyAskedQuestion/static/js/2.40b1d22a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FrequentlyAskedQuestion/static/js/2.40b1d22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f9ec453e6c593c3c7d4",
+    "url": "/FrequentlyAskedQuestion/static/js/main.8117f0a4.chunk.js"
+  },
+  {
+    "revision": "2cd5cd765e5ff3f3b9df",
+    "url": "/FrequentlyAskedQuestion/static/js/runtime-main.3317a106.js"
+  }
+]);
